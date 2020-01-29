bcf16550faac6598c45734e91a799fd4c9736637
Brandon Powell <brandon.powell@armory.io>
2020-01-28 16:21:29 -0800

    feat(*): can overrride manifeststatus

---
+++ src/layout/ManifestStatus/ManifestStatus.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+interface IProps{};
+
+// From app/scripts/modules/kubernetes/src/v2/pipelines/stages/deployManifest/manifestStatus/ManifestStatus.tsx
+//  in OSS Deck
+export class ManifestStatus extends React.Component<IProps> {
+  public render() {
+    return (<div>Hello World</div>);
+  }
+}
---
+++ src/spinnaker.modules/manifestStatusShim/index.ts
@@ -0,0 +1,1 @@
+export * from './manifeststatus.shim.module';
